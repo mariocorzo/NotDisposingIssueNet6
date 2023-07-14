d65faa6a679fce4efbc04a6a3dd75b0f0741f142
Mario Corzo <mcorzo@hchb.com>
2023-07-14 12:21:45 -0300

    It adds a component and dispose it

---
+++ NotDisposingIssueNet6/Pages/Issue.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace NotDisposingIssueNet6.Pages
+{
+    public partial class Issue
+    {
+        public bool ShowComponent;
+
+        public void ShowComponentButtonClicked()
+        {
+			Console.WriteLine("Button clicked!");
+			ShowComponent = !ShowComponent;
+        }
+
+        public string GetShowComponentButtonText()
+        {
+            return ShowComponent ? "Hide Component" : "Show Component";
+        }
+    }
+}
